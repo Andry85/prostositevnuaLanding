b4772794356434cebc2594641a160b1c00d5e172
HOME\Andry <shapovala@ukr.net>
2018-03-25 22:19:36 +0300

    add files

--- src/js/main.js
+++ src/js/main.js
@@ -50,4 +50,23 @@
         });
     });
 
+
+    $('.worksSlider.owl-carousel').owlCarousel({
+        loop:true,
+        margin: 28,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items: 2,
+            },
+            1000:{
+                items: 3,
+                loop:false
+            }
+        }
+    });
+
 })(jQuery);
